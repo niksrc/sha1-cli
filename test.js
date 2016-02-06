18f027cf0d00dfa90c6ea23ded7cd54fc6e559a5
Nikhil Srivastava <niksrc@gmail.com>
2016-02-06 15:23:22 +0530

    add basic tests ftw

--- test.js
+++ test.js
@@ -1,6 +1,19 @@
 import test from 'ava';
 import fn from './';
 
-test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
+var sha1sum = '6663746aea8cb18b78c1792297b4eedc2d7d2187';
+var invalid = 'file not found';
+
+test.cb('calculate sha1', t => {
+	fn('./lib/sha1.js', {sha1: true}, (sum) => {
+		t.is(sum, sha1sum);
+		t.end();
+	});
+});
+
+test.cb('fail for non existing files', t => {
+	fn('./lib/sha1s.js', {sha1: true}, (sum) => {
+		t.is(sum, invalid);
+		t.end();
+	});
 });
